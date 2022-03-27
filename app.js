ca359c1ac882c98f0004ab69afe99eb4b7ff62bb
KevinFrutos <vinke.14297@gmail.com>
2022-03-27 13:31:12 +0200

    Añadido el dominio del front en la config de los cors

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ["http://localhost:5500", "http://localhost:3000"],
+		origin: ["http://localhost:5500", "http://localhost:3000", "https://socialemote-front.web.app"],
 		credentials: true,
 	})
 );
