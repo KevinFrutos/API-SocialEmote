61f55dd666cdcf9e7bff6ed12433afe94e37cd93
KevinFrutos <vinke.14297@gmail.com>
2023-03-19 12:29:36 +0100

    main: Deleted WS from API

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const { WebSocketServer } = require("ws");
-const server = require("http").createServer();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 //DATABASE
 const connection = require("./controllers/database");
-const Publication = require("./models/schema_publication");
-const Session = require("./models/schema_sessions");
 
 //ROUTES
 const register = require("./routes/register");
@@ -64,45 +60,6 @@ app.use("/user", verifyToken, deletePublication);
 app.use("/user", verifyToken, addLike);
 app.use("/user", verifyToken, unLike);
 app.use("/user", verifyToken, addComment);
-
-// WEBSOCKET SERVER IMPLEMENTATION
-const wsServer = new WebSocketServer({ server: server });
-
-// POR CADA REQUEST DE UN CLIENTE, SE CREA UN NUEVO WEBSOCKET
-server.on("request", app);
-
-// OBJETO QUE GUARDA LOS CLIENTES CONECTADOS
-const users = {};
-
-wsServer.on("connection", (client, req) => {
-	const user = req.headers.cookie?.split(" ").filter(user => user.includes("user="))[0]?.split("=")[1].split(";")[0];
-	users[user] = client;
-	Publication.watch([], { fullDocument: "updateLookup" }).on("change", async () => {
-		const cursor = await Publication.aggregate([{ $sort: { publication_date: -1 } }, { $limit: 20 }]);
-		const data = [];
-		for await (const doc of cursor) {
-			data.push(doc);
-		}
-		client.send(
-			JSON.stringify({
-				type: "publicaciones",
-				publicaciones: data,
-			})
-		);
-	});
-	Session.watch([], { fullDocument: "updateLookup" }).on("change", async change => {
-		if (change.operationType === "delete") {
-			const cursor = await Session.find({ user });
-			if (cursor.length === 0) {
-				users[user].send(
-					JSON.stringify({
-						type: "session",
-					})
-				);
-			}
-		}
-	});
-});
 
 // SERVER LISTENING
 server.listen(process.env.PORT, () => {
